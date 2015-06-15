0b997f964b206adc423f01100891157eb138e0c2
tpratama <theopratama@live.com>
2015-06-15 14:23:28 +0700

    Error Handling 404 ADDED

---
+++ application/views/error.php
@@ -0,0 +1,25 @@
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+ 
+
+  
+    <!-- About Section -->
+    <section id="error" class="container err text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>404 - Halaman Web Hilang Terbawa Angin!</h2>
+                <p >Mohon maaf atas kesalahan yang terjadi, silahkan kembali ke portal page.</p>
+                <a href="<?php base_url(); ?>/warkop-TC" class="btn btn-default btn-lg">Redirect</a>
+               
+            </div>
+             
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; Warkop-TC 2015</p>
+        </div>
+    </footer>
